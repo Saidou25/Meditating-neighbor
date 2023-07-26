adb676e3b69679a74d4640125d8f8f15b6d6d5b4
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-07-26 06:32:51 -0400

    added .env and secret

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const secret = "mysecretssshhhhhhh"; // process.env.SECRET
+const secret = "mysecretssshhhhh"; // process.env.SECRET
 const expiration = "2h";
 
 module.exports = {
